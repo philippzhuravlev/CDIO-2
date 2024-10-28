1ef10600ebe1fa216063934241d1817562fe7d0f
Philipp Craine <125037167+philippzhuravlev@users.noreply.github.com>
2024-10-28 11:42:41 +0100

    added checkLanguage()
    +added some useless comments

--- src/main/java/com/cdio2/Main.java
+++ src/main/java/com/cdio2/Main.java
@@ -13,24 +13,22 @@ class Main {
 }
 
 class Game {
+    // init game objects and vars
     private Scanner scanner = new Scanner(System.in); 
-    private Boolean isPlayer1Turn = true; 
     private Player player1 = new Player("Player 1", 1000);
     private Player player2 = new Player("Player 2", 1000);
     private Wallet player1Wallet = new Wallet("Player 1", 1000);
     private Wallet player2Wallet = new Wallet("Player 2", 1000);
+    private boolean isPlayer1Turn = true;
     private boolean hasExtraTurn = false;
 
     public void start() {
-
-        System.out.println("Choose language (en for english, da for danish):");
-        String langChoice = scanner.nextLine();
-        String langCode = langChoice.equals("da") ? "da" : "en";
-        Tiles.loadLanguage(langCode);
-
         this.scanner = new Scanner(System.in);
         scanner.useLocale(java.util.Locale.ENGLISH);
 
+        checkLanguage();
+
+        // main game loop
         while (true) {
             if (isPlayer1Turn) { 
                 doPlayerTurn(player1, player1Wallet, null); 
@@ -45,6 +43,20 @@ class Game {
             }
         }
         scanner.close();
+    }
+
+    public void checkLanguage() {
+        System.out.println("Choose language (en for english, da for danish):");
+        String langChoice = scanner.nextLine();
+
+        String langCode;
+        if (langChoice.equals("da")) {
+            langCode = "da"; 
+        } else {
+            langCode = "en"; 
+        }
+        
+        Tiles.loadLanguage(langCode); 
     }
 
     public Boolean isGameWon() {
@@ -94,8 +106,6 @@ class Game {
         return hasExtraTurn;
     }
 }
-
-
 
 class Player {
     private String name;
